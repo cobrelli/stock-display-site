ab68cdc7fa8f8526ef47086788486c854b760e32
Tomi Virtanen <cobrelli@gmail.com>
2013-12-01 13:10:13 +0200

    print rename,header,footer

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php echo "Toimii" ?>
+
+<?php 
+#header('Content-Type: text/html; charset=utf-8');
+
+require_once('assets/header.php');
+
+require_once('read-data-to-json.php');
+
+require_once('assets/footer.php');
+?>
 
