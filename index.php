45cb50314c2bf921da965e04fcec1e6f844a43a8
Tomi Virtanen <cobrelli@gmail.com>
2013-12-03 13:41:20 +0200

    nätix

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 
 <?php 
-#header('Content-Type: text/html; charset=utf-8');
-
 require_once('assets/header.php');
 
 require_once('read-data-to-json.php');
 #echo $json;
-#print_r($stocks);
-echo "<div style='width:75%; border-radius:15px;background-color:red;padding:5%'>";
-echo "<div style='width: 100%; height: 50%; min-height: 400px; overflow: scroll; background-color:lime'><table border='1'>";
-#$i = 0;
+echo "<div style='width:75%; -moz-border-radius: 15px; border-radius:15px;background-color:red;padding:2%'>";
+echo "<div style='width: 100%; height: 50%; min-height: 400px; overflow: scroll; background-color:white'><table border='1'>";
 
 echo "<tr>";
 echo "<td></td>";
@@ -18,7 +14,6 @@ foreach ($stocks as $val) {
 		continue;
 	}
 	echo "<td>" . $val[0] . "</td>";
-	#$i++;
 }
 echo "</tr>";
 
@@ -26,17 +21,12 @@ for ($i=1; $i < count($stocks[0]); $i++) {
 	echo "<tr>";
 	$date = explode("|", $stocks[0][$i]);
 	echo "<td>" . $date[0] . "</td>";
-	#echo $i;
 		foreach ($stocks as $val) {
 			if(empty($val[1])){
 				continue;
 			}
-			# code...
 			$value = explode("|", $val[$i]);
 			echo "<td>" . $value[1] . "</td>";
-			#echo "<td>" . $val[$i] . "</td>";
-			#echo "<td>" . explode("|", $val[$i]) . "</td>";
-			#$i++;
 		}
 	echo "</tr>";
 }
